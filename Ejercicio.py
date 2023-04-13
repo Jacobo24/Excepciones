4dbd28dcc338a90a50407bf1e67e4a0a479a51eb
Jacobo24 <jaccalgar@gmail.com>
2023-04-13 13:55:55 +0200

    comentarios

--- Ejercicio.py
+++ Ejercicio.py
@@ -6,9 +6,9 @@ class Correo():
 
 
     def validarCorreo(self):
-        patron = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+        patron = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$" # es lo que sirve para validar el correo
         correo = input("-> Ingrese su correo: ")
-        if re.search(patron, correo):
+        if re.search(patron, correo): # el bucle para ver si el correo es váloido o no
             if self.correo == correo:
                 print("-> Correo valido")
                 print("-> Hola", correo)
@@ -17,7 +17,7 @@ class Correo():
         else:
             print("Correo invalido")
 
-correo = ["jacobo@gmail.com", "maria@gmail.com"]
+correo = ["jacobo@gmail.com", "maria@gmail.com"] # lista para que poner los correos que quieras que sean válidos
 
 for i in correo:
     try:
