1d5274717c34dca2f2c9a2edfd9567c0d6d528ff
Jacobo24 <jaccalgar@gmail.com>
2023-04-13 11:53:53 +0200

    1 parte del ejercicio hecho

--- Ejercicio.py
+++ Ejercicio.py
@@ -1,1 +1,20 @@
 import re
+
+class Correo():
+    def __init__(self, correo):
+        self.correo = correo
+
+
+    def validarCorreo(self):
+        patron = "^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+        correo = input("Ingrese su correo: ")
+        if re.search(patron, self.correo):
+            if self.correo == correo:
+                print("Correo valido")
+            else:
+                raise Exception("Correo inválido")
+        else:
+            print("Correo invalido")
+
+correo1 = Correo("jacobo@gmail.com")
+correo1.validarCorreo()
